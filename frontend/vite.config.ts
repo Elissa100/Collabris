1b53a87bbb6f88540f9c5c16a026d83cca1ac5ab
Elissa Sibomana <sibomanaelissa71@gmail.com>
2025-10-13 16:52:21 +0200

    feat(frontend): update vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,35 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-
-    define: {
-    global: {},
-  },
-  
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
+      "/api": {
+        target: "http://localhost:8080",
         changeOrigin: true,
         secure: false,
       },
-      '/ws': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
+      "/ws": {
+        target: "http://localhost:8080",
         ws: true,
+        changeOrigin: true,
       },
     },
-  },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
   },
 });
