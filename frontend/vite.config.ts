78fd4dd2fe6bcd301a40b581de4158f64331067f
Elissa Sibomana <sibomanaelissa71@gmail.com>
2025-10-13 11:23:43 +0200

    feat(frontend): update vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,11 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
+    define: {
+    global: {},
+  },
+  
   plugins: [react()],
   resolve: {
     alias: {
