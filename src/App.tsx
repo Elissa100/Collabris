c695c75988728f4f6a5d868a8c06835c445ac777
unknown <moon@collabris.dev>
2026-03-04 10:04:12 +0200

    Update App.tsx to use new LandingPage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
+import LandingPage from './LandingPage';
 
 function App() {
-  return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <p>Start prompting (or editing) to see magic happen :)</p>
-    </div>
-  );
+  return <LandingPage />;
 }
 
 export default App;
